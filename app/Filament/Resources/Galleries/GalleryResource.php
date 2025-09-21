d71d4540d4f114b74ca552e83b281d147842be1b
Kang Ruswan <ruswan@ummi.ac.id>
2025-09-21 17:40:58 +0700

    Update GalleryResource navigation icon to use Heroicon::Photo

--- app/Filament/Resources/Galleries/GalleryResource.php
+++ app/Filament/Resources/Galleries/GalleryResource.php
@@ -22,7 +22,7 @@ class GalleryResource extends Resource
 {
     protected static ?string $model = Gallery::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::Photo;
 
     protected static ?string $recordTitleAttribute = 'title';
 
