36538aac5b75cd305b60eaf2673fc195ca3bf30f
Kang Ruswan <ruswan@ummi.ac.id>
2025-09-22 06:21:13 +0700

    Add theme styles and about page template for Filament front
    - Created a new CSS file for theme styles, importing existing styles and defining custom classes for the top bar.
    - Added a new Blade template for the About page, utilizing the Filament panel component.

---
+++ app/Filament/Front/Pages/About.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Front\Pages;
+
+use Filament\Pages\Page;
+
+class About extends Page
+{
+    protected string $view = 'filament.front.pages.about';
+}
--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,7 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    input: [
+    'resources/css/filament/admin/theme.css',
+    ]
 });
