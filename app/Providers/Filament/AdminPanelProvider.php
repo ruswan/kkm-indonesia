64388645a721da4dbc83f1f06da8e05280e960e9
Kang Ruswan <ruswan@ummi.ac.id>
2025-09-22 11:32:31 +0700

    Enhance Admin Panel: Add top navigation and adjust topbar styles for improved layout

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,8 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->spa()
+            ->topNavigation()
             ->colors([
                 'primary' => Color::Amber,
             ])
