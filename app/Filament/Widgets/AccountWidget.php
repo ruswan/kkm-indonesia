60e417a952ef8eb74a50d792cd62f13203fdf354
ruswan <sukabumikode@gmail.com>
2025-10-01 09:14:32 +0700

    Enhance StatsOverview widget: add statistics for Gallery and Articles, update URLs for navigation, and improve overall widget functionality.

---
+++ app/Filament/Widgets/AccountWidget.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\AccountWidget as Widget;
+
+class AccountWidget extends Widget
+{
+    protected static ?int $sort = -3;
+
+    protected static bool $isLazy = false;
+
+    /**
+     * @var view-string
+     */
+    protected string $view = 'filament-panels::widgets.account-widget';
+}
--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,8 +2,15 @@
 
 namespace App\Filament\Widgets;
 
+use App\Filament\Resources\Articles\ArticleResource;
+use App\Filament\Resources\Counselors\CounselorResource;
+use App\Filament\Resources\Events\EventResource;
+use App\Filament\Resources\Galleries\GalleryResource;
+use App\Filament\Resources\Provinces\ProvinceResource;
+use App\Models\Article;
 use App\Models\Counselor;
 use App\Models\Event;
+use App\Models\Gallery;
 use App\Models\Province;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
@@ -13,15 +20,26 @@ class StatsOverview extends StatsOverviewWidget
     protected function getStats(): array
     {
         return [
-            Stat::make(__('Members'), Counselor::count())
+            Stat::make(__('Counselors'), Counselor::count())
                 ->icon('heroicon-o-users')
-                ->color('success'),
+                ->color('success')
+                ->url(CounselorResource::getUrl('index')),
+            Stat::make(__('Gallery'), Gallery::count())
+                ->icon('heroicon-o-photo')
+                ->color('danger')
+                ->url(GalleryResource::getUrl('index')),
+            Stat::make(__('Articles'), Article::count())
+                ->icon('heroicon-o-document')
+                ->color('danger')
+                ->url(ArticleResource::getUrl('index')),
             Stat::make(__('Provinces'), Province::count())
                 ->icon('heroicon-o-map')
-                ->color('primary'),
+                ->color('primary')
+                ->url(ProvinceResource::getUrl('index')),
             Stat::make(__('Events'), Event::count())
                 ->icon('heroicon-o-calendar')
-                ->color('warning'),
+                ->color('warning')
+                ->url(EventResource::getUrl('index')),
         ];
     }
 }
